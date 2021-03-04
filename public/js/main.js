88e1a7e78d95cc65e7229aa22e7d200c32c9bfe9
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-04 13:39:20 -0300

    Primeiros passos com jQuery

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,5 @@
-console.log('Hello, jQuery!');
+var fraseEl = $('.frase').text();
+var tamanhoFraseEl = $('#tamanho-frase');
+
+var numPalavras = fraseEl.split(' ').length;
+tamanhoFraseEl.text(numPalavras);
