08dd91167807bbc847994a5469c6c7d287ae2c1b
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-04 14:18:02 -0300

    Eventos do jQuery

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,18 @@
 var fraseEl = $('.frase').text();
 var tamanhoFraseEl = $('#tamanho-frase');
 
-var numPalavras = fraseEl.split(' ').length;
-tamanhoFraseEl.text(numPalavras);
+var campoDigitacaoEl = $('.campo-digitacao');
+var contadorCaracteresEl = $('#contador-caracteres');
+var contadorPalavrasEl = $('#contador-palavras');
+
+var numPalavrasFrase = fraseEl.split(' ').length;
+
+tamanhoFraseEl.text(numPalavrasFrase);
+
+campoDigitacaoEl.on('input', function () {
+  var numCaracteresDigitadas = campoDigitacaoEl.val().length;
+  var numPalavrasDigitadas = campoDigitacaoEl.val().split(/\S+/).length - 1;
+
+  contadorCaracteresEl.text(numCaracteresDigitadas);
+  contadorPalavrasEl.text(numPalavrasDigitadas);
+});
